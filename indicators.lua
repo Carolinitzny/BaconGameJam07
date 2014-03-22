fbdf55e9b5a64c73c52a19cf1e7de74f2122bcce
Julia Hertel <3hertel@informatik-uni.hamburg.de>
2014-03-22 21:09:14 +0100

    nicer gauge

--- indicators.lua
+++ indicators.lua
@@ -14,7 +14,13 @@ function Indicators:draw()
        end
     end
     love.graphics.draw(images.gauge, love.graphics.getWidth()-75, 80, 0, 0.15, 0.15, images.gauge:getWidth()/2, images.gauge:getHeight()/2)
-    love.graphics.draw(images.needle, love.graphics.getWidth()-75, 80, angle, 0.15, 0.15, images.needle:       getWidth()/2, images.needle:getHeight()/2)
+    if plane.fuel > 0 then
+        love.graphics.draw(images.needle, love.graphics.getWidth()-75, 80, angle, 0.15, 0.15, images.needle:getWidth()/2,
+        images.needle:getHeight()/2)
+    else
+        love.graphics.draw(images.needle, love.graphics.getWidth()-75, 80, 0, 0.15, 0.15, images.needle:getWidth()/2,
+        images.needle:getHeight()/2)
+    end
     love.graphics.setColor(255, 255, 255)
     for i = 0, plane.quantity-1 do
         local r = math.sin(time*2*math.pi)*0.2
