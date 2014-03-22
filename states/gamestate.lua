b82733f585b88a8564c954da117da9bb0e21102c
Carolin Konietzny <3konietz@informatik.uni-hamburg.de>
2014-03-23 00:59:39 +0100

    Get the airports back

--- states/gamestate.lua
+++ states/gamestate.lua
@@ -28,7 +28,7 @@ function GameState:update(dt)
 
     -- Generating villages
     local distance = 2000
-    local size = 400
+    local size = 2000
     local g = self.generated
 
     -- left
@@ -94,10 +94,10 @@ function GameState:generateVillages(left, right, top, bottom)
     end
     --airports
 
-   local density = 0.15/(800*600)
+   local density = 0.1/(800*600)
     local area = math.abs((right - left)* (bottom - top))
-    local count = area * density
-    
+    local count = math.ceil(area * density)
+    print("Generating " .. count .. " airports")
     for k = 1, count do
         for l = 1, 50 do
             local pos = Vector:new(math.random(left,right), math.random(top, bottom))
