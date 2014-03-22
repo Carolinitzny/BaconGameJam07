15551387067ed8940e32c8b510a6627a3ee10854
Julia Hertel <3hertel@informatik-uni.hamburg.de>
2014-03-22 19:39:37 +0100

    counting scores

--- entities/package.lua
+++ entities/package.lua
@@ -10,6 +10,7 @@ function Package:initialize(plane)
     tween(1.0, self, {speed = 0}, "inQuad")
     tween(1, self, {altitude = 0}, "inQuad", function() 
         self.state:add(SmokeRing:new(self.position:clone()))
+        self:landed()
     end)
 end
 
@@ -25,5 +26,15 @@ function Package:update(dt)
     dir:rotate(self.direction)
     dir = dir*dt*self.speed
     self.position = self.position + dir
+end
+
+function Package:landed()
+    for k, v in pairs(self.state.world) do
+        if v:isInstanceOf(Target) then
+            if math.abs(self.position.x - v.position.x) <= 50 and math.abs(self.position.y - v.position.y) <= 50 then
+                self.state:addScore(1)
+            end
+        end  
+    end        
 end
 
