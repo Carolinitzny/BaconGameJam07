ed1508b01c2d19366c18763aec031ef9c4ac1a44
Julia Hertel <3hertel@informatik-uni.hamburg.de>
2014-03-23 19:26:01 +0100

    moving tornados. veeeery bad performance...

--- states/gamestate.lua
+++ states/gamestate.lua
@@ -143,7 +143,7 @@ function GameState:generateWorld(left, right, top, bottom)
     end
     
     --tornado
-    local density = 0.01/(800*600)
+    local density = 0.5/(800*600)
     local count = area*density
     for k = 1, count do
         local pos = Vector:new(math.random(left,right), math.random(top, bottom))
