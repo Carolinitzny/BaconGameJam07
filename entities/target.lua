bdbb77500ff19a515f2f87aea0b43a7b4df4d399
Julia Hertel <3hertel@informatik-uni.hamburg.de>
2014-03-23 02:24:13 +0100

    nicer targets

--- entities/target.lua
+++ entities/target.lua
@@ -2,16 +2,18 @@ local class = require 'middleclass'
 
 require "entity"
 Target = class ("Village", Entity)
-Target.z = 3
+Target.z = 1
 
 function Target:initialize(x,y,village)
     self.position = Vector:new(x,y) 
-    self.size = 10
+    self.size = 15
     self.village = village
 end
 
 function Target:draw()
+    love.graphics.setColor(0, 255, 0, 20)
+    love.graphics.circle("fill", self.position.x, self.position.y, self.size*5) 
     love.graphics.setColor(0, 255, 0)
-    love.graphics.circle("fill", self.position.x, self.position.y, self.size) 
+    love.graphics.circle("fill", self.position.x, self.position.y+10, self.size)
     love.graphics.setColor(255, 255, 255)    
 end
